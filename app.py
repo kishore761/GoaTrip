36b6607262f6851be846894769bf6e5c1f98d688
kishore761 <119946514+kishore761@users.noreply.github.com>
2026-05-28 11:02:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+registrations = []
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        place = request.form['place']
+        members = request.form['members']
+
+        registrations.append({
+            'name': name,
+            'phone': phone,
+            'place': place,
+            'members': members
+        })
+
+    return render_template('index.html', registrations=registrations)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
